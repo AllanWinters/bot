e3d5ed23461b9dd7d8f28ce692173e9602eac76d
AllanWinters <70565234+AllanWinters@users.noreply.github.com>
2021-03-13 19:41:23 +0700

    Update primbon.js

--- menu/primbon.js
+++ menu/primbon.js
@@ -2,11 +2,11 @@ const a = '```'
 const primbon = (prefix, pushname2, groupName, user, name) => {
 return `
 
-╭─────≽「 *REGULATION ${name}* 」
+╭─────≽「 *USER INFO* 」
 ┴
 ┣⊱  ${a}NAMA USER:${a} *${pushname2}*
 ┣⊱  ${a}NAMA GRUP:${a} *${groupName}*
-┣⊱  ${a}VERSION:${a} *0.0.0*
+┣⊱  ${a}VERSION:${a} *1.0.0*
 ┣⊱  ${a}USER TERDAFTAR:${a} *${user.length} User*
 ┬
 ╰────────────────────────
